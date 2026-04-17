0b17d003b6651845d5043bf0b809cedb6aa12459
Frank-Deng-WH6HS <enter_the_dragon@foxmail.com>
2026-04-17 19:10:30 +0800

    更新程序包cresda_metadata_parse. (2026-04-17)
    * 添加对CBERS-04卫星MUX传感器L2级数据的支持.

---
+++ Process/Python_Package/cresda_metadata_parse/auxiliary/__init__.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+#CRESDA 卫星光学影像产品辅助文件参数
+
+class OpticalL1A(object): 
+    
+    RPB = r"\g<archive>.rpb"; 
+    
+class OpticalL2(object): 
+    
+    VIEW_GEOM = r"\g<archive>-SatAngle.txt"; 
+    
--- Process/Python_Package/cresda_metadata_parse/cb04/__init__.py
+++ Process/Python_Package/cresda_metadata_parse/cb04/__init__.py
@@ -3,15 +3,25 @@
 
 import re; 
 
-from .. import metadata, utils; 
+from .. import auxiliary as aux; 
+from .. import path; 
 
-#影像产品名格式
-
-PRODUCT_NAME_FILTER = {
-    "MUX": r"(?P<archive>CB04\-MUX\-[0-9]+\-[0-9]+\-2[0-9]{7}\-L2[0-9]{10})", 
-    "IRS": r"(?P<archive>CB04\-IRS\-[0-9]+\-[0-9]+\-2[0-9]{7}\-L2[0-9]{10})", 
-}; 
-PRODUCT_NAME_FILTER = {
-    key: re.compile(val) 
-    for key, val in PRODUCT_NAME_FILTER.items()
-}; 
+#20米多光谱相机 (MUX)
+class MUX(path.FilenameMatcher, aux.OpticalL2): 
+    
+    SRC_ARX_BASENAME = \
+        r"(?P<archive>" \
+            "CB04\-MUX(?:\-[0-9]+){2}\-[0-9]{8}" \
+            "\-L20*(?P<prod>[1-9][0-9]{,10})" \
+        ")/";  
+    SRC_ARX_PREFIX = SRC_ARX_BASENAME + r"(?P=prod)/(?P=archive)\.TIF"; 
+    TRG_ARX_BASENAME = r"\g<archive>/\g<prod>/"; 
+    TRG_MATADATA_SUFFIX = r"\g<archive>.XML"; 
+    
+    def __init__(self): 
+        super(type(self).__mro__[0], self).__init__(); 
+        self.source_pattern = self.SRC_ARX_PREFIX; 
+        self.target_patterns = (
+            self.TRG_ARX_BASENAME + self.TRG_MATADATA_SUFFIX, 
+            self.TRG_ARX_BASENAME + self.VIEW_GEOM
+        ); 
